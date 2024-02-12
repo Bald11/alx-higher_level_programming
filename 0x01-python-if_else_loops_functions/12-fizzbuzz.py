0d126ad75997bce021397283ea621e8a5fad7ded
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2024-02-12 17:26:45 +0000

    fizzbuzz problem

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,12 +6,13 @@ def fizz(fizz):
 def buzz(buzz):
     return buzz % 5 == 0
 
-for i in range(1,101):
-    if fizz(i) and buzz(i):
-        print("FizzBuzz", end=' ')
-    elif fizz(i):
-        print("Fizz", end=' ')
-    elif buzz(i):
-        print("Buzz", end=' ')
-    else:
-        print(i, end=' ')
+def fizzbuzz():
+    for i in range(1, 101):
+        if fizz(i) and buzz(i):
+            print("FizzBuzz", end=' ')
+        elif fizz(i):
+            print("Fizz", end=' ')
+        elif buzz(i):
+            print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
