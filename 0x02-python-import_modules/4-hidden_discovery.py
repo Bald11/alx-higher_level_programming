386a7268be6fa133291f0214a92329f982b0cec1
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2024-02-13 21:32:56 +0000

    importing and listing defines

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    hidden = dir(hidden_4)
+
+    for name in sorted(hidden):
+        if not name.startswith('__'):
+            print(name)
