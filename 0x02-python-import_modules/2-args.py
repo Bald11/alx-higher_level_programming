6ca336a0459054f5ea5c619e5fc86df7c1a5b491
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2024-02-13 19:53:05 +0000

    counting and printing argvs

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    output = ''
+    if (len(sys.argv) == 1):
+        output += '0 arguments.\n'
+    elif (len(sys.argv) > 1):
+        if (len(sys.argv) == 2):
+            output += '1 argument:\n1: {}\n'.format(sys.argv[1])
+        else:
+            output += '{} arguments:\n'.format(len(sys.argv))
+            for i in range(1, len(sys.argv)):
+                output += '{}: {}\n'.format(i, sys.argv[i])
+    print(output, end='')
